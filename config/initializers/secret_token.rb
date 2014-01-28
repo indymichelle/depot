a106ead3e09a97f264f2ddce5b58432f4ac46986
Michelle Madden <maddenmichelle@yahoo.com>
2014-01-27 19:35:17 -0500

    Started basic shopping site, created index view for products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '0826a0b090bf85ba2aceb7133f3e803e792521f3e3f116ebc62baaae20a96a32796d3e8a0b3dc2e61ad74fe2f6f8ef1da011e7954ba2b74edd9bc6d997c2a868'
